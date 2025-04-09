8477becb4e48f7452b21d13de229da61bea268b6
Adelin <adelinfarcas01@gmail.com>
2025-04-09 20:32:50 +0300

    add: css, app + new component Todo

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const DATA = [
+  {name: 'Eat', id: 'todo-0', completed: true },
+  {name: 'Sleep', id: 'todo-1', completed: false },
+  {name: 'Repeat', id: 'todo-2', completed: false },
+]
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App 
       subject="Milo"
       greeting="Welcome to my humble abode"
+      tasks={DATA}
      />
   </StrictMode>,
 )

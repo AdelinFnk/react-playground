c6db7fe609b741a7a59b451ef3a6d5cb6d099d68
Adelin <adelin.farcas@proton.me>
2025-04-12 16:08:01 +0300

    add events and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
 
 function App(props) {
-  console.log(props)
-  const taskList = props.tasks?.map((task) => (
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks?.map((task) => (
     <Todo 
       name={task.name}
       id={task.id}
@@ -12,11 +17,19 @@ function App(props) {
       key={task.id}
     />
   ));
-  
+
+  function addTask(name) {
+    //name is like name: name inside the obj
+    //nanoid generates an unique string for each id with the prefix todo-
+    const newTask = { name, id: `todo-${nanoid()}`, completed: false };
+    setTasks([...tasks, newTask]);
+    console.log([...tasks, newTask])
+  }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-        <Form />
+        <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,42 @@
-function Form() {
+import { useState } from "react";
+
+function Form(props) {
+
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name) 
+    setName("")
+  };   
+
+  //to not alert when the input is empty
+  /* function handleSubmit(event) {
+    event.preventDefault();
+
+    // Trim the name to remove any leading or trailing whitespace
+    const trimmedName = name.trim();
+
+    trimmedName ? props.addTask(trimmedName) : null;
+    setName("");
+  } */
+
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
           </label>
         </h2>
         <input
+          value={name}
+          onChange={handleChange}
           type="text"
           id="new-todo-input"
           className="input input__lg"
